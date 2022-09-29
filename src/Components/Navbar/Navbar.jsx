2878d7c7c9df4237e47cc4d9d19094fc70ee7b9a
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-09-29 20:43:04 -0300

    add tipografia

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 function Navegador() {
   return (
       <Navbar bg="light" expand="lg">
-        <Container>
+        <Container className='navbar-margin'>
           <Navbar.Brand as={Link} to="/">Yoha & Nahui</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
